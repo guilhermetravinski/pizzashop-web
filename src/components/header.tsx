c79111199a637ba28301f4e51849c6e3918c3859
Guilherme Travinski Ferreira <ti.guilhermet@unimedpg.com.br>
2024-11-14 15:29:10 -0300

    Something

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
-import { Link } from 'react-router-dom'
 
+import AccountMenu from './account-menu'
 import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
 import { Separator } from './ui/separator'
 
 export function Header() {
@@ -22,6 +23,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
